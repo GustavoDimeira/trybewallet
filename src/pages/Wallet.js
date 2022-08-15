8f57b908dd17a068ae807e5378ac61877bf27824
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-08-15 13:45:54 -0400

    req-6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Wallet extends React.Component {
         <h1>TrybeWallet</h1>
         <Header />
         <WalletForm />
+        <Table />
       </div>
     );
   }
